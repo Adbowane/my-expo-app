70fa98e3c56090f0c7f5fc35817080702ae1ff20
Abdoulaye Wane <abdoulaye.wane@saint-benigne.fr>
2025-03-06 12:22:43 +0100

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { ScreenContent } from 'components/ScreenContent';
+import { EditScreenInfo } from 'components/EditScreenInfo';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
       <ScreenContent title="Home" path="App.tsx" />
+      <EditScreenInfo path="App.tsx" />
       <StatusBar style="auto" />
     </>
   );
